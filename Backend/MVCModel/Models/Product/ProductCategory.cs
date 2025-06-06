aef55d9cca402cd04e4d93050ad883b129267acc
vam404 <108697223+vam404@users.noreply.github.com>
2025-06-05 22:03:45 -0500

    Models and Base APIs

---
+++ Backend/MVCModel/Models/Product/ProductCategory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCModel.Models.Product
+{
+    internal class Category
+    {
+    }
+}
