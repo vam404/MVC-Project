c66c8f69fd0ec0849206c34e956d4fb0ed86cc3a
vam404 <108697223+vam404@users.noreply.github.com>
2025-06-05 19:54:00 -0500

    Initial commit

---
+++ Backend/MVCApp/Controllers/ProductController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+
+    }
+}
